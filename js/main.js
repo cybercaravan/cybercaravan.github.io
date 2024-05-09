44695acdef8443cfd4b97b9a9368473f838d6a98
mhmunir <mhmunir914794@gmail.com>
2024-05-10 00:13:07 +0600

    google form

--- js/main.js
+++ js/main.js
@@ -51,23 +51,23 @@ $('.video-owl').owlCarousel({
 })
 
 
-const optionMenu = document.querySelector(".select-menu"),
-  selectBtn = optionMenu.querySelector(".select-btn"),
-  options = optionMenu.querySelectorAll(".option"),
-  sBtn_text = optionMenu.querySelector(".sBtn-text");
+// const optionMenu = document.querySelector(".select-menu"),
+//   selectBtn = optionMenu.querySelector(".select-btn"),
+//   options = optionMenu.querySelectorAll(".option"),
+//   sBtn_text = optionMenu.querySelector(".sBtn-text");
 
-selectBtn.addEventListener("click", () =>
-  optionMenu.classList.toggle("active")
-);
+// selectBtn.addEventListener("click", () =>
+//   optionMenu.classList.toggle("active")
+// );
 
-options.forEach((option) => {
-  option.addEventListener("click", () => {
-    let selectedOption = option.querySelector(".option-text").innerHTML;
-    sBtn_text.innerHTML = selectedOption;
+// options.forEach((option) => {
+//   option.addEventListener("click", () => {
+//     let selectedOption = option.querySelector(".option-text").innerHTML;
+//     sBtn_text.innerHTML = selectedOption;
 
-    optionMenu.classList.remove("active");
-  });
-});
+//     optionMenu.classList.remove("active");
+//   });
+// });
 
 
 
